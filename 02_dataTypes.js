7aa2338eff057da8f10a9edad8648a0ff36d3702
Hariom yadav <hyadhuvansi14@gmail.com>
2025-12-10 20:34:44 +0530

    Document JavaScript data types in 02_dataTypes.js
    Added examples of different data types in JavaScript.

--- 02_dataTypes.js
+++ 02_dataTypes.js
@@ -1,2 +1,18 @@
 "use strict";//treat all JS code as newer version
-// alert(3 + 3); we are using nodejs not browser
+
+// alert(3 + 3); we are using nodejs not browser 
+
+let name="Hitesh"  //string datatype
+let age =28 //number datatype
+let isloggedIn = false // boolean datatype
+
+
+// number => 2 to power 53
+// bigInt => use for large numder
+// string  =>use under single or double cotes ' ' or " "
+// boolean => true or false
+// null =>standalone value
+// undefined => value is not asigned or not given
+// symbol =>usally use for uniqueness (use mostally in react)
+// object
+console.log(typeof name)
