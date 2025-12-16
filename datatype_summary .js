885ebbc7bbfdce7a1ecd3f400e05065f2dab146a
Hariom yadav <hyadhuvansi14@gmail.com>
2025-12-16 10:25:52 +0530

    datatype

---
+++ datatype_summary .js
@@ -0,0 +1,40 @@
+// primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100; // number
+const scoreValue = 100.3; // number
+
+const isLoggedIn = false; // boolean
+const outsideTemp = null; // object
+let userEmail;
+
+const id = Symbol("12345"); // symbol
+const anotherId = Symbol("12345"); // symbol
+console.log(id === anotherId); // false
+const bigNumber = 1234567890123456789012345678901234567890n; // bigint
+
+// reference(non - primitive) data types - objects
+
+// array,functions,objects
+
+const heros = ["shaktiman", "naagraj", "doga"]; // array - object
+const myobject = {
+    name: "Hariom",
+    age: 19,
+};
+const myFunction = function () {
+    console.log("hello world");
+};
+
+
+console.log(typeof score);
+console.log(typeof scoreValue);
+console.log(typeof isLoggedIn);
+console.log(typeof outsideTemp);
+console.log(typeof userEmail);
+console.log(typeof id);
+console.log(typeof bigNumber);
+console.log(typeof heros);
+console.log(typeof myobject);
+console.log(typeof myFunction);
