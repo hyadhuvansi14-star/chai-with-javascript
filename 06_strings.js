3d3085e758b4272b6c575c5e7c79113afbfc2a68
Hariom yadav <hyadhuvansi14@gmail.com>
2026-01-14 15:02:22 +0530

    strings of javascript

---
+++ 06_strings.js
@@ -0,0 +1,88 @@
+// const name = "Alice";
+// const repocount = 42;
+// // console.log(name + repocount + " repositories");
+// expected output: "Alice42 repositories"
+
+// console.log(`Hello my name is ${name} and I have ${repocount} repositories.`);
+// expected output: "Hello my name is Alice and I have 42 repositories."
+// const gameName = new String('hitesh');
+// expected output: [String: 'hitesh']
+// console.log(gameName[0]);
+// expected output: 'h'
+// console.log(gameName.__proto__);
+// expected output: String {...}
+//
+// console.log(gameName.length);
+// expected output: 6
+// console.log(gameName.toUpperCase());
+// expected output: 'HITESH'
+// console.log(gameName.charAt(5));
+// expected output: 'h'
+// console.log(gameName.indexOf('sh'));
+// expected output: 3
+
+
+
+
+
+//Achor Method
+// const contentString = "Hello, world";
+
+// innerHTML = contentString.anchor("hello");
+// console.log(innerHTML);
+// expected output: <a href="#hello">Hello, world</a>
+// at method
+
+// const sentence = "The quick brown fox jumps over the lazy dog.";
+
+// let index = 5;
+
+// console.log(`An index of ${index} returns the character ${sentence.at(index)}`);
+// expected output: An index of 5 returns the character q
+// big method
+
+// const contentString = "Hello, world";
+// innerHTML = contentString.big();
+// console.log(innerHTML);
+// expected output: <big>Hello, world</big>
+
+// bold method
+
+// const contentString = "Hello, world";
+// innerHTML = contentString.bold();
+// console.log(innerHTML);
+// expected output: <b>Hello, world</b>
+
+// char at method
+// const contentString = "Hello, world";
+// innerHTML = contentString.charAt(4);
+// console.log(innerHTML);
+// expected output: o
+
+// char code at method
+// const contentString = "Hello, world";
+// innerHTML = contentString.charCodeAt(10);
+// console.log(innerHTML);
+// expected output: 114
+
+// concat method
+// const str1 = "Hello";
+// const str2 = "World!";
+// const str3 = str1.concat(" ", str2);
+// console.log(str3); // Expected output: "Hello World!"
+
+
+// endsWith method
+
+// const str1 = "Cats are the best!";
+
+// console.log(str1.endsWith("best!"));
+// Expected output: true
+
+// console.log(str1.endsWith("best", 17));
+// Expected output: true
+
+// const str2 = "Is this a question?";
+
+// console.log(str2.endsWith("question"));
+// Expected output: false
