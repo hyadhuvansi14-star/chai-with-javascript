368f9fcf6553db814037ef6f77131b32e6539ce4
Hariom yadav <hyadhuvansi14@gmail.com>
2026-01-14 15:02:56 +0530

    nums and maths

---
+++ 07_nums_and_maths.js
@@ -0,0 +1,68 @@
+// const score = 100;
+// console.log(score); // 100
+// const bonus = new Number(20);
+// console.log(bonus); // [Number: 20]
+// console.log(bonus.toString().length); // 2
+// console.log(bonus.toFixed(2)); // 20.00
+// console.log(bonus.toFixed(3)); // 20.000
+
+
+// const otherNumber = 150.5678;
+// console.log(otherNumber.toPrecision(3));// 151
+// console.log(otherNumber.toPrecision(4));// 150.6
+// console.log(otherNumber.toPrecision(5));// 150.57
+
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-US')); // 10,000,000
+// console.log(hundreds.toLocaleString('en-IN'));// 1,00,00,000
+
+// ********************* MATHS ***********************
+
+// console.log(Math);
+
+// abs working as mod of maths|100|
+// console.log(Math.abs(-100)); // 100
+// console.log(Math.abs(100)); // 100
+
+
+// round working as rounding off the decimal values
+// console.log(Math.round(4.6)); // 5
+// console.log(Math.round(4.3)); // 4
+// console.log(Math.round(-4.6)); // -5
+// console.log(Math.round(-4.3)); // -4
+
+
+
+// ceil always rounds up the value
+// console.log(Math.ceil(4.1)); // 5
+// console.log(Math.ceil(4.9)); // 5
+// console.log(Math.ceil(-4.1)); // -4
+// console.log(Math.ceil(-4.9)); // -4
+
+// floor always rounds down the value
+// console.log(Math.floor(4.1)); // 4
+// console.log(Math.floor(4.9)); // 4
+// console.log(Math.floor(-4.1)); // -5
+// console.log(Math.floor(-4.9)); // -5
+
+// min or max use for identify smaller or bigger number in the list
+// console.log(Math.min(2, 6, 4, -2, 0));
+// console.log(Math.max(2, 6, 4, -2, 0));
+
+
+// math random use for generate a random number between 0 to 1
+
+// console.log(Math.random()); // 0.0 to 0.9999999999
+// console.log((Math.random() * 10) + 1); // 1 to 10.9999999
+// console.log(Math.floor((Math.random() * 10) + 1)); // 1 to 10
+
+// generate random number between min to max
+
+// const min = 10;
+// const max = 20;
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));//10 to 20
+
+
+
+
+
